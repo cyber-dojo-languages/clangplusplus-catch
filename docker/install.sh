bff519b1d8d13d7f186deb1026ef3ba70a0bdfcc
Jon Jagger <jon@jaggersoft.com>
2020-03-19 20:58:27 +0000

    Upgrade to catch 2.11.3

--- docker/install.sh
+++ docker/install.sh
@@ -7,7 +7,7 @@ curl \
    --remote-header-name  `# -J Use the header-provided filename` \
    --location            `# -L Follow redirects` \
    --remote-name         `# -O Write output to a file named as the remote file` \
-     https://github.com/catchorg/Catch2/releases/download/v2.11.1/catch.hpp \
+     https://github.com/catchorg/Catch2/releases/download/v2.11.3/catch.hpp \
 
 apt-get remove --yes curl
 cd /usr/src
