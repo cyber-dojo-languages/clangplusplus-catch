438ae54f98c30c65fcb40b620071e7e863c40c6f
Tahir Adil <tahir@kosli.com>
2025-07-25 14:26:27 +0200

    Add github workflows, update catch

--- docker/install.sh
+++ docker/install.sh
@@ -7,7 +7,7 @@ curl \
    --remote-header-name  `# -J Use the header-provided filename` \
    --location            `# -L Follow redirects` \
    --remote-name         `# -O Write output to a file named as the remote file` \
-     https://github.com/catchorg/Catch2/releases/download/v2.11.3/catch.hpp \
+     https://github.com/catchorg/Catch2/releases/download/v3.9.0/catch_amalgamated.hpp \
 
 apt-get remove --yes curl
 cd /usr/src
