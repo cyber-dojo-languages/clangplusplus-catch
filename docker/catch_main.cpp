397fbaf3a463f70ca597a4af09b1cd6bf2e195ae
JonJagger <jon@kosli.com>
2025-07-29 13:01:03 +0100

    Docker: install hpp and cpp file

--- docker/catch_main.cpp
+++
@@ -1,4 +0,0 @@
-// In a Catch project with multiple files, dedicate one file to compile the
-// source code of Catch itself and reuse the resulting object file for linking.
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch_amalgamated.hpp>
--- docker/install.sh
+++ docker/install.sh
@@ -3,13 +3,16 @@
 apt-get install --yes curl
 mkdir /usr/include/catch2
 cd /usr/include/catch2
+
 curl \
    --remote-header-name  `# -J Use the header-provided filename` \
    --location            `# -L Follow redirects` \
    --remote-name         `# -O Write output to a file named as the remote file` \
-     https://github.com/catchorg/Catch2/releases/download/v3.9.0/catch_amalgamated.hpp \
+     https://github.com/catchorg/Catch2/releases/download/v3.9.0/catch_amalgamated.hpp
 
-apt-get remove --yes curl
-cd /usr/src
-clang++ -fPIC -Wall -c catch_main.cpp
-mv /usr/src/catch_main.o /usr/lib
+curl \
+   --remote-header-name  `# -J Use the header-provided filename` \
+   --location            `# -L Follow redirects` \
+   --remote-name         `# -O Write output to a file named as the remote file` \
+     https://github.com/catchorg/Catch2/releases/download/v3.9.0/catch_amalgamated.cpp
+
